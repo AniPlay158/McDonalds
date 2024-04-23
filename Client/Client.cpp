0731d47cf06846ebe322cda118a4043691bcc732
user <user@LILIT>
2024-04-23 10:50:15 +0300

    Добавьте файлы проекта.

---
+++ Client/Client.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+#include <queue>
+#include <vector>
+#include <string>
+#include <thread>
+#include <mutex>
+#include <windows.h>
+
+using namespace std;
+
+struct Order
+{
+    string dishName;
+    int cookTime;
+};
+
+class OrderProcessor
+{
+private:
+    queue<Order> orderQueue;
+    mutex mtx;
+    vector<string> availableDishes = { "Чизбургер", "Чикен рол", "кола", "Дабл чизбургер", "Апельсиновий сiк", "Роял Делюкс", "Биг тейсті" };
+    const int maxCookingCapacity = 10;
+
+public:
+
+    void addOrder(const Order& order)
+    {
+        mtx.lock();
+        orderQueue.push(order);
+        mtx.unlock();
+    }
+
+
+    void processOrders()
+    {
+
+        while (!orderQueue.empty())
+        {
+
+            mtx.lock();
+            if (orderQueue.empty())
+            {
+
+                mtx.unlock();
+                continue;
+
+            }
+
+            Order order = orderQueue.front();
+            orderQueue.pop();
+            mtx.unlock();
+
+
+            auto it = find(availableDishes.begin(), availableDishes.end(), order.dishName);
+
+            if (it != availableDishes.end())
+            {
+
+                cout << "Приготовлено: " << order.dishName << "\n";
+
+                this_thread::sleep_for(chrono::seconds(order.cookTime));
+                cout << "Страва '" << order.dishName << "' готова, приємного апетиту!\n";
+                Sleep(3000); 
+                system("cls");
+            }
+            else
+            {
+
+                cout << "Страву '" << order.dishName << "' не можна приготувати.\n";
+            }
+        }
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    OrderProcessor processor;
+
+    processor.addOrder({ "Чизбургер", 3 });
+    processor.addOrder({ "кола", 2 });
+    processor.addOrder({ "Чикен рол", 4 });
+    processor.addOrder({ "Дабл чизбургер", 3 });
+    processor.addOrder({ "Апельсиновий сiк", 2 });
+    processor.addOrder({ "Роял Делюкс", 5 });
+    processor.addOrder({ "Биг тейсті", 5 });
+
+    thread orderThread(&OrderProcessor::processOrders, &processor);
+
+    orderThread.join();
+
+    return 0;
+}
---
+++ Server/Server.cpp
@@ -0,0 +1,116 @@
+﻿#include <winsock2.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <thread>
+#include <mutex>
+
+using namespace std;
+
+#define MAX_CLIENTS 10
+#define DEFAULT_BUFLEN 4096
+
+#pragma comment(lib, "ws2_32.lib") // Winsock library
+#pragma warning(disable:4996)
+
+SOCKET server_socket;
+
+vector<string> history;
+
+struct Order
+{
+    string dishName;
+    int cookTime;
+};
+
+mutex mtx;
+
+void addOrder(const Order& order)
+{
+    mtx.lock();
+    history.push_back(order.dishName);
+    mtx.unlock();
+}
+
+void processOrders()
+{
+    while (true)
+    {
+        mtx.lock();
+        if (history.empty())
+        {
+            mtx.unlock();
+            continue;
+        }
+
+        string order = history.front();
+        history.erase(history.begin());
+        mtx.unlock();
+
+        cout << "Received order: " << order << endl;
+
+        // Process the order
+        // In this example, we're just printing out the order information
+        // In a real application, you would perform the necessary processing
+        // Here, we're just sleeping for a short time to simulate processing
+        this_thread::sleep_for(chrono::seconds(3));
+        cout << "Order processed: " << order << endl;
+    }
+}
+
+int main()
+{
+    cout << "Start server... DONE." << endl;
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        cerr << "Failed. Error Code: " << WSAGetLastError() << endl;
+        return 1;
+    }
+
+    // Create a socket
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+        cerr << "Could not create socket: " << WSAGetLastError() << endl;
+        return 2;
+    }
+
+    // Prepare the sockaddr_in structure
+    sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(8888);
+
+    // Bind socket
+    if (bind(server_socket, (sockaddr*)&server, sizeof(server)) == SOCKET_ERROR) {
+        cerr << "Bind failed with error code: " << WSAGetLastError() << endl;
+        return 3;
+    }
+
+    // Listen to incoming connections
+    listen(server_socket, MAX_CLIENTS);
+
+    cout << "Server is waiting for incoming connections..." << endl;
+
+    // Start processing thread
+    thread orderThread(processOrders);
+
+    while (true) {
+        SOCKET new_socket; // New client socket
+        sockaddr_in address;
+        int addrlen = sizeof(sockaddr_in);
+        if ((new_socket = accept(server_socket, (sockaddr*)&address, &addrlen)) < 0) {
+            cerr << "Accept function error" << endl;
+            return 5;
+        }
+
+        cout << "New connection, socket fd is " << new_socket << ", ip is: " << inet_ntoa(address.sin_addr) << ", port: " << ntohs(address.sin_port) << endl;
+
+        // Add new socket to array of sockets
+        mtx.lock();
+        history.push_back("New connection established");
+        mtx.unlock();
+    }
+
+    closesocket(server_socket);
+    WSACleanup();
+    return 0;
+}
